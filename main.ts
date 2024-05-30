226c25a96b0ec7a385d6447433f33e0d5a6bd0ca
Filip Groh <146720108+Filip-Groh@users.noreply.github.com>
2024-05-30 17:34:41 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+const numberOfHolesForRotation: number = 20
+
+let isOn: boolean = false
+let holeCount: number = 0
+
+basic.forever(() => {
+    let state: boolean = pins.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 0, 1) >= 0.5
+    if (isOn !== state) {
+        isOn = state
+        holeCount++
+    }
+    console.logValue("Pin1 - Raw", pins.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 0, 1))
+    console.logValue("Pin1 - Pure", state ? 1 : 0)
+    console.logValue("Pin1 - Count", holeCount)
+    console.logValue("Pin1 - Rotation", holeCount / numberOfHolesForRotation)
+
+    basic.pause(1)
+})
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function() {
+    holeCount = 0
+})
+
+PCAmotor.MotorRun(PCAmotor.Motors.M1, 200)
