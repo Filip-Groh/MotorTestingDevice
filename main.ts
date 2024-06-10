d8c16a72e69f9bd67f108fa25ee6f19646cf28b9
Filip Groh <146720108+Filip-Groh@users.noreply.github.com>
2024-06-10 12:01:28 +0200

    queue fix

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 const numberOfHolesForRotation: number = 20
+const lengthOfRPSQueue: number = 10
 const lengthOfRotationQueue: number = 10
 
 const rpsInMs: number = 100
@@ -8,6 +9,8 @@ const loggingRateInMs: number = 250
 let holeCount: number = 0
 let rotation: number = 0
 let rps: number = 0
+
+let rpsQueue: Queue = new Queue(lengthOfRPSQueue, [])
 
 let rotations: Queue = new Queue(lengthOfRotationQueue, [])
 
@@ -21,16 +24,19 @@ pins.onPulsed(DigitalPin.P1, PulseValue.High, () => {
 })
 
 loops.everyInterval(rpsInMs, function () {
+    rps = (rotation - lastRotation) * 1000 / rpsInMs
+loops.everyInterval(rpsInMs, function () {
     rotations.push(rotation)
     rotations.getAverage()
     rps = (rotation - lastRotation) * 4
     lastRotation = rotation
+    rpsQueue.push(rps)
 })
 
 loops.everyInterval(loggingRateInMs, () => {
     console.logValue("Pin1 - Count", holeCount)
     console.logValue("Pin1 - Rotation", rotation)
-    console.logValue("Pin1 - RPS", rps)
+    console.logValue("Pin1 - RPS", rpsQueue.getAverage())
 })
 
 input.onLogoEvent(TouchButtonEvent.Pressed, function() {
