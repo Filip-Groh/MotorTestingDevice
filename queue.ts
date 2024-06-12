5d554330cb2c51a6e7e80e7101a84d4ffc989bdd
Filip Groh <146720108+Filip-Groh@users.noreply.github.com>
2024-06-12 19:17:07 +0200

    Linearity

--- queue.ts
+++ queue.ts
@@ -15,15 +15,33 @@ class Queue {
         return null
     }
 
-    getAverage(): number {
+    getSum(): number {
         let sum: number = 0
-        for(let i: number = 0; i < this.data.length; i++) {
+        for (let i: number = 0; i < this.data.length; i++) {
             sum += this.data[i]
         }
-        return sum / this.data.length
+        return sum
     }
 
     getLength(): number {
         return this.data.length
+    }
+
+    getAverage(): number {
+        return this.getSum() / this.data.length
+    }
+
+    getLinearity(a: number, b: number): number {
+        let sum: number = 0
+        for (let i: number = 0; i < this.data.length; i++) {
+            sum += Math.abs(this.data[i] - a * i + b)
+        }
+        return sum
+    }
+
+    getFirstPointLastPointLinearity(): number {
+        let a: number = (this.data[0] - this.data[this.data.length - 1]) / this.data.length
+        let b: number = this.data[0]
+        return this.getLinearity(a, b)
     }
 }
